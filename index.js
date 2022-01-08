2a81c6a7a2bb8bc843abe695e46c58545b63e163
Andres F Jaramillo Valdes <andresjar10@gmail.com>
2022-01-08 14:30:29 -0500

    Se hicieron ajustes sobre el archivo index.js para mejorar y simplificar su codigo, ademas se le agregaron las extensiones png e ico para poder desplegarlas, se agrego el favicon a todas las paginas.

--- index.js
+++ index.js
@@ -1,54 +1,47 @@
 const path = require('path')
 const {createReadStream} = require('fs')
 const {createServer} = require('http')
-/* var fs = require('fs'); */
 
 // configuramos con una variable de entorno el puerto
 const {PORT = 3000} = process.env
-
-const HTML_CONTENT_TYPE = 'text/html'
-const CSS_CONTENT_TYPE = 'text/css'
-const JS_CONTENT_TYPE = 'text/javascript'
-const SVG_CONTENT_TYPE = 'image/svg+xml'
 
 const PUBLIC_FOLDER = path.join(__dirname, '')
 // creamos un requestListener para pasarle a nuestro servidor
 const requestListener = (req, res) => {
   const {url} = req
   let statusCode = 200
-  let contentType = HTML_CONTENT_TYPE
+  let contentType = 'text/html'
   let stream
 
   // si estamos pidiendo la ruta principal, devolvemos el contenido del index.html
   if (url === '/') {
     stream = createReadStream(`${PUBLIC_FOLDER}/index.html`)
-  } else if (url.match("\.svg$")) { // para los archivos en img
-    contentType = SVG_CONTENT_TYPE
-    /* stream = fs.readFileSync(`${PUBLIC_FOLDER}${url}`) */
   } else if (url.match("\.css$")) { // para los archivos CSS
-    contentType = CSS_CONTENT_TYPE
+    contentType = 'text/css'
   } else if (url.match("\.js$")) { // para los archivos JavaScript
-    contentType = JS_CONTENT_TYPE
-  } else { // si llegamos aquí, es un 404
-    statusCode = 404
+    contentType = 'text/javascript'
+  } else if (url.match("\.svg$")) { // para los archivos svg
+    contentType ='image/svg+xml'
+  } else if (url.match("\.png$")) { // para los archivos png
+    contentType = 'image/png'
+  } else if (url.match("\.ico$")) { // para los archivos png
+    contentType = 'image/x-icon'
+    stream = createReadStream(`${PUBLIC_FOLDER}/img/favicon.ico`)
   }
 
-  if(url !== '/'){
+  if(url !== '/' && !url.match("\.ico$")){
     stream = createReadStream(`${PUBLIC_FOLDER}${url}`)
   }
 
-  // escribimos las cabeceras de la respuesta dependiendo de la request
-  res.writeHead(statusCode, {'Content-Type': contentType})
-  // si tenemos un stream, lo enviamos a la respuesta
   if (stream) {
-      /* if (contentType === SVG_CONTENT_TYPE ) {
-        res.end(stream, 'binary');
-      } else { */
-        stream.pipe(res)
-      /* } */
-  } 
-  // si no, devolvemos un string diciendo que no hemos encontrado nada
-  else {
+    // escribimos las cabeceras de la respuesta dependiendo de la request
+    res.writeHead(statusCode, {'Content-Type': contentType})
+    // como tenemos un stream, lo enviamos a la respuesta
+    stream.pipe(res)
+
+  } else { // si no, devolvemos un string diciendo que no hemos encontrado nada
+    statusCode = 404
+    res.writeHead(statusCode, {'Content-Type': contentType})
     return res.end('Not found')
   }
 }
